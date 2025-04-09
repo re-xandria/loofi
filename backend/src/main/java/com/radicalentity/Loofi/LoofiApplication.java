1a5949a6c70f236f158d29c21ca00aa074ef232a
Xandria Crosland <xandria.crosland@gmail.com>
2025-04-09 16:50:39 -0400

    init commit

---
+++ backend/src/main/java/com/radicalentity/Loofi/LoofiApplication.java
@@ -0,0 +1,13 @@
+package com.radicalentity.Loofi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoofiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoofiApplication.class, args);
+	}
+
+}
