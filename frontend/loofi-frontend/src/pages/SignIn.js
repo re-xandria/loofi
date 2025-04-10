89cc469d855bdf08de8ac89422e03757e8b24891
Xandria Crosland <xandria.crosland@gmail.com>
2025-04-10 16:16:18 -0400

    US 1.2 - create sign in page - B (GUI, scalable design)  - v1.2

--- frontend/loofi-frontend/src/pages/SignIn.js
+++ frontend/loofi-frontend/src/pages/SignIn.js
@@ -5,26 +5,28 @@ import placeholder from './placeholder.png';
 
 function SignIn() {
     return(
-        <Container className="App" fluid>
-            <Row >
-                <Col sm={3} style={{marginRight:10 + "em"}} >
-                        <Image style={{height:100 + 'vh', width:40 + "vw"}} src={placeholder} alt="placeholder" />
+
+        <Container fluid style={{height:100+"vh"}}>
+            <Row style={{ height: "100%" }}>
+                <Col style={{ padding: 0, height: "100%" }}>
+                    <Image fluid
+                           style={{ width: "100%", height: "100%", objectFit: "cover" }} src={placeholder} alt="placeholder" />
                 </Col>
                 {/* pattern and logo on left*/}
-                <Col style={{padding: "auto", margin: "auto", textAlign:"left"}}  id="sign-in" sm={4} >
+                <Col fluid style={{ margin: "auto", textAlign:"left", paddingInlineStart:20 + "em", paddingInlineEnd:20 + "em"}}  id="sign-in" xxl={7} >
                     <h1>Sign In</h1>
                     <p>New to Loofi? <a href="#">Create an account</a></p>
-                    <form>
+                    <form style={{marginRight:10 + "rem"}}>
                         <div style={{marginTop:2.5 + "rem"}}>
-                            <label>Username or Email Address *</label>
-                            <input type="text" id="usernameEmail"/>
+                            <label style={{display:"block", marginBottom:.5 + "rem"}}>Username or Email Address *</label>
+                            <input style={{display:"block", paddingTop:.75 + "rem", paddingBottom:.75 + "rem", paddingLeft:1 + "rem", paddingRight:1 + "rem", width: 20 + "rem", marginBottom:3 + "rem"}} type="text" id="usernameEmail"/>
                         </div>
                         <div>
-                            <label>Password *</label>
-                            <input type="text" id="usernameEmail"/>
-                            <a href="">Forgot Password?</a>
+                            <label style={{display:"block", marginBottom:.5 + "rem"}}>Password *</label>
+                            <input style={{display:"block", paddingTop:.75 + "rem", paddingBottom:.75 + "rem", paddingLeft:1 + "rem", paddingRight:1 + "rem", width: 20 + "rem" }} type="text" id="usernameEmail"/>
+                            <a style={{display:"block",  marginTop:.5 + "rem", marginBottom:3 + "rem", width: 20 + "rem"}} href="">Forgot Password?</a>
                         </div>
-                        <button type="submit">Sign In</button>
+                        <button style={{paddingInlineStart:1 + "rem", paddingInlineEnd:1 + "rem", paddingBlockStart: .75+ "rem", paddingBlockEnd: .75+ "rem", width: 20 + "rem" }} type="submit">Sign In</button>
                     </form>
                 </Col>
             </Row>
