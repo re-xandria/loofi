34c9cd64dd4e924afa25652b37f8e86a2d2b769e
Xandria <xcrosl1@wgu.edu>
2025-05-06 16:27:53 -0400

    US 2.2 - create game page - v1.3, finish roshambo with all features

--- frontend/loofi-frontend/src/features/games/Roshambo.js
+++ frontend/loofi-frontend/src/features/games/Roshambo.js
@@ -1,4 +1,4 @@
-import {Button, Col, Container, Row} from "react-bootstrap";
+import {Button, Col, Container, Row, Spinner} from "react-bootstrap";
 import {useEffect, useRef, useState} from "react";
 
 function Roshambo() {
@@ -13,61 +13,71 @@ function Roshambo() {
     const [computerChoice, setComputerChoice] = useState(-1);
     let [playerScore, setPlayerScore] = useState(0);
     let [computerScore, setComputerScore] = useState(0);
-    const [roundNumber, setRoundNumber] = useState(1);
+    const [roundNumber, setRoundNumber] = useState(0);
     const roundMessage = useRef(null);
+    const [isRoundOver, setIsRoundOver] = useState(false);
+    const choices = ["Rock", "Paper", "Scissors"];
+    const [showSpinner, setShowSpinner] = useState(false);
 
     const startGame = () => {
         setIsStarted((isStarted) => !isStarted);
     }
 
     useEffect(() => {
-        // 0 is rock, 1 is paper, 2 is scissor
-
-        // when option pressed store value in player choice get computer choice
-        setComputerChoice(Math.floor(Math.random() * 3))
-    }, [playerChoice]);
+        if (playerChoice === -1 || computerChoice === -1) return;
+        if (isRoundOver) updateScore();
+    }, [isRoundOver]);
 
     useEffect(() => {
-        //console.log("PC: " + playerChoice + " | CC: " + computerChoice)
+        let timer;
+        if (showSpinner) {
+             timer = setTimeout(() => {
+                 setShowSpinner(false);
+                 setIsRoundOver(true)
+            }, 2000)
+        }
+        return () => clearTimeout(timer);
+    }, [showSpinner]);
 
-        // (optional) display "computer's turn!" and wait 1-2 seconds
-        // display message saying who won
-        if (playerChoice === -1 || computerChoice === -1) return;
-        updateScore();
-        endRound();
-    }, [playerChoice, computerChoice]);
+    const getChoices = (playerChoiceNum) => {
+        // 0 is rock, 1 is paper, 2 is scissor
+        setPlayerChoice(playerChoiceNum)
+        setComputerChoice(Math.floor(Math.random() * 3))
+        roundMessage.current.innerText = "Computer is thinking...";
+        setShowSpinner(true);
+        setIsRoundOver(false)
+    }
+
+    const startNewRound = () => {
+        setIsRoundOver(false)
+        setRoundNumber(roundNumber + 1)
+        roundMessage.current.innerText = "Your Turn"
+    }
 
     const updateScore = () => {
-        // add 1 to winner's score
-
         switch (playerChoice - computerChoice) {
             case 1:
-                roundMessage.current.innerText = "You won!"
+                roundMessage.current.innerText = `You Won with ${choices[playerChoice]}!`
                 setPlayerScore(playerScore + 1)
                 break;
             case 2:
-                roundMessage.current.innerText = "Computer won!"
-                setPlayerScore(computerScore + 1)
+                roundMessage.current.innerText = `Computer Won with ${choices[computerChoice]}!`
+                setComputerScore(computerScore + 1)
                 break;
             case -1:
-                roundMessage.current.innerText = "Computer won!"
-                setPlayerScore(computerScore + 1)
+                roundMessage.current.innerText = `Computer Won with ${choices[computerChoice]}!`
+                setComputerScore(computerScore + 1)
                 break;
             case -2:
-                roundMessage.current.innerText = "You won!"
+                roundMessage.current.innerText = `You Won with ${choices[playerChoice]}!`
                 setPlayerScore(playerScore + 1)
                 break;
             default:
-                roundMessage.current.innerText = "It's a tie!"
+                roundMessage.current.innerText = `It's a Tie with ${choices[playerChoice]}!`
+                setPlayerScore(playerScore + 1)
+                setComputerScore(computerScore + 1)
+                break;
         }
-    }
-
-
-
-    const endRound = () => {
-        // if "play again?" selected, start the game again and display new score
-
-        setRoundNumber(roundNumber + 1)
     }
 
     return(
@@ -76,24 +86,27 @@ function Roshambo() {
 
                 {!isStarted && <Button variant={"outline-light"} className={"px-4 py-3"} onClick={startGame} id="start-button">Start Game</Button>}
                 {isStarted && <Container className={"d-flex flex-column h-100 w-100 text-center"}>
-                    <Row className={"d-flex justify-content-start align-items-start w-100 py-5 text-white"}>
+                    <Row className={"d-flex justify-content-start align-items-start w-100 py-5 text-white mt-3 mb-2"}>
                         <Col><h5>Player Score</h5><h5 id="player-score">{playerScore}</h5></Col>
                         <Col><h5>Round </h5><h5>{roundNumber}</h5></Col>
                         <Col><h5>Computer Score</h5><h5 id="computer-score">{computerScore}</h5></Col>
                     </Row>
-                    <h3 ref={roundMessage} className={"mb-4 text-white"}>Your Turn!</h3>
-                    <Row id="player-options" className={"d-flex justify-content-center mx-auto py-5"}>
-                        <Col>
-                            <Button variant={"outline-light"} className={"px-4 py-3"} onClick={e => {setPlayerChoice(0)}}>Rock</Button>
-                        </Col>
-                        <Col>
-                            <Button variant={"outline-light"} className={"px-4 py-3"} onClick={e => {setPlayerChoice(1)}}>Paper</Button>
-                        </Col>
-                        <Col>
-                            <Button variant={"outline-light"} className={"px-4 py-3"} onClick={e => {setPlayerChoice(2)}}>Scissor</Button>
-                        </Col>
-                        {/*{roundOver && <Button>Play Again?</Button>*/}
+                    <Row className={"d-flex flex-column gap-2 my-2 text-white align-items-center"}>
+                        <h3 ref={roundMessage}>Your Turn</h3>
+                        {showSpinner && <Spinner animation={"border"} as="span"></Spinner>}
                     </Row>
+                    {!isRoundOver && <Row id="player-options" className={"d-flex justify-content-center mx-auto py-5"}>
+                        <Col>
+                            <Button variant={"outline-light"} className={"px-4 py-3"} onClick={e => {getChoices(0)}}>Rock</Button>
+                        </Col>
+                        <Col>
+                            <Button variant={"outline-light"} className={"px-4 py-3"} onClick={e => {getChoices(1)}}>Paper</Button>
+                        </Col>
+                        <Col>
+                            <Button variant={"outline-light"} className={"px-4 py-3"} onClick={e => {getChoices(2)}}>Scissor</Button>
+                        </Col>
+                    </Row>}
+                    {isRoundOver && <Button variant={"light"} className={"d-inline-block px-4 py-3 mx-auto mt-4"} onClick={startNewRound}>Play Again</Button>}
                 </Container>}
 
             </Container>
